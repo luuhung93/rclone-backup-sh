67c119f206af35117564e812e7de76213c9e0c5c
A Hung Hung <luuhung93@gmail.com>
2020-07-03 11:32:54 +0700

    fist commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # backup-sh
+
+#install
+https://rclone.org/install/
+curl https://rclone.org/install.sh | sudo bash
+
+#config
+#https://rclone.org/drive/
+rclone config
---
+++ unity.sh
@@ -0,0 +1,37 @@
+#Backup Server and Upload to Cloud
+
+#!/bin/bash
+
+SERVER_NAME=UNITY
+
+TIMESTAMP=$(date +"%F")
+BACKUP_DIR="/root/backup/$TIMESTAMP"
+
+SECONDS=0
+
+mkdir -p "$BACKUP_DIR/mongo"
+
+echo "Starting Backup Database";
+mongodump --archive="$BACKUP_DIR/dump.gz" --gzip --db=unity
+echo "Finished";
+echo '';
+
+echo "Starting Backup Nginx Configuration";
+cp -r /etc/nginx/conf.d/ $BACKUP_DIR/nginx/
+echo "Finished";
+echo '';
+
+size=$(du -sh $BACKUP_DIR | awk '{ print $1}')
+
+echo "Starting Uploading Backup";
+/usr/sbin/rclone move $BACKUP_DIR "remote:$SERVER_NAME/$TIMESTAMP" >> /var/log/rclone.log 2>&1
+# Clean up
+rm -rf $BACKUP_DIR
+/usr/sbin/rclone -q --min-age 1w delete "remote:$SERVER_NAME" #Remove all backups older than 2 week
+/usr/sbin/rclone -q --min-age 1w rmdirs "remote:$SERVER_NAME" #Remove all empty folders older than 2 week
+/usr/sbin/rclone cleanup "remote:" #Cleanup Trash
+echo "Finished";
+echo '';
+
+duration=$SECONDS
+echo "Total $size, $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
